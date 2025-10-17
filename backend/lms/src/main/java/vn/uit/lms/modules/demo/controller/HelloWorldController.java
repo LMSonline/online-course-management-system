a1a6f5b684990e28c1ff1de9c449a314bda1b527
TrinhHoa-23520508 <23520508@gm.uit.edu.vn>
2025-10-17 10:32:05 +0700

    refactor(infrastructure): optimize folder structure

--- backend/lms/src/main/java/vn/uit/lms/modules/demo/controller/HelloWorldController.java
+++ backend/lms/src/main/java/vn/uit/lms/modules/demo/controller/HelloWorldController.java
@@ -1,4 +1,4 @@
-package vn.uit.lms.controller;
+package vn.uit.lms.modules.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
