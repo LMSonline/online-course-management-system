5c2f0871eedce6029afac8c3f6c8b8b14f9751f5
TrinhHoa-23520508 <23520508@gm.uit.edu.vn>
2025-10-18 11:31:04 +0700

    chore: config cors and base security

---
+++ backend/lms/src/main/java/vn/uit/lms/config/SecurityConfiguration.java
@@ -0,0 +1,37 @@
+package vn.uit.lms.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        String[] whiteList = {"/ping", "/api/v1/auth/**",
+                "/storage/**", "/v3/api-docs/**",
+                "/swagger-ui/**",
+                "/swagger-ui.html"};
+        http.csrf(c -> c.disable())
+                .cors(Customizer.withDefaults())
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers(whiteList)
+                        .permitAll()
+                        .anyRequest().authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
+    }
+}
