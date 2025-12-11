e15e3b4f56497c644d506e89d6905e316c73ee25
TrinhHoa-23520508 <23520508@gm.uit.edu.vn>
2025-12-11 10:56:01 +0700

    feat: enhance logic auth service

---
+++ backend/lms/src/main/java/vn/uit/lms/shared/exception/TooManyRequestsException.java
@@ -0,0 +1,8 @@
+package vn.uit.lms.shared.exception;
+
+public class TooManyRequestsException extends RuntimeException {
+    public TooManyRequestsException(String message) {
+        super(message);
+    }
+}
+
